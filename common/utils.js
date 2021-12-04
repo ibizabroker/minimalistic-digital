172bee6ed118901034da79209e424a205562b1f5
tarun-gowda <tarun.gowda2000@gmail.com>
2021-12-04 22:06:20 +0530

    utils update

--- common/utils.js
+++ common/utils.js
@@ -3,4 +3,52 @@ export function zeroPad(i) {
       i = "0" + i;
     }
     return i;
-  }
+}
+
+export function dayOfWeek(i) {
+    switch (i) {
+      case 0:
+        return "SUN";
+      case 1:
+        return "MON";
+      case 2:
+        return "TUE";
+      case 3:
+        return "WED";
+      case 4:
+        return "THU";
+      case 5:
+        return "FRI";
+      case 6:
+        return "SAT";
+    }
+}
+
+export function nameOfMonth(i) {
+    switch (i) {
+      case 0:
+        return "JAN";
+      case 1:
+        return "FEB";
+      case 2:
+        return "MAR";
+      case 3:
+        return "APR";
+      case 4:
+        return "MAY";
+      case 5:
+        return "JUN";
+      case 6:
+        return "JUL";
+      case 7:
+        return "AUG";
+      case 8:
+        return "SEP";
+      case 9:
+        return "OCT";
+      case 10:
+        return "NOV";
+      case 11:
+        return "DEC";
+    }
+}
