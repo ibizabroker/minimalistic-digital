0535817c41bc9ff3a7f6d1644619d856d03e7b3a
tarun-gowda <tarun.gowda2000@gmail.com>
2021-12-05 18:05:40 +0530

    user settings

--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,53 @@
-console.log('Hello world!');
+import * as messaging from "messaging";
+import { settingsStorage } from "settings";
+import { DEFAULT_MODEL } from "../common/constants";
+
+messaging.peerSocket.onopen = () => {
+  restoreSettings();
+};
+
+messaging.peerSocket.onclose = () => {
+  
+};
+
+settingsStorage.onchange = evt => {
+  let data = {
+    key: evt.key,
+    newValue: evt.newValue
+  };
+  sendVal(data);
+};
+
+function restoreSettings() {
+  if (settingsStorage.length === 0) {
+    restoreDefaults();
+  }
+
+  for (let index = 0; index < settingsStorage.length; index++) {
+    let key = settingsStorage.key(index);
+    if (key) {
+      let data = {
+        key: key,
+        newValue: settingsStorage.getItem(key)
+      };
+      sendVal(data);
+    }
+  }
+}
+
+function restoreDefaults() {
+  console.log('Restoring default values for Companion');
+
+  const keys = Object.keys(DEFAULT_MODEL)
+
+  keys.forEach(key => {
+    console.log(`${key} | ${DEFAULT_MODEL[key]}`);
+    settingsStorage.setItem(key, DEFAULT_MODEL[key]);
+  })
+}
+
+function sendVal(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  }
+}
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,13 +1,52 @@
+import { APP_NAME, DATE_FORMATS } from "../common/constants"
+
 registerSettingsPage(({ settings }) => (
   <Page>
     <Section
-      title={
-        <Text bold align="center">
-          App Settings
-        </Text>
-      }
+      title=
+        {
+          <Text bold align="center">
+            {`${APP_NAME} Customisations`}
+          </Text>
+        }
     >
-      <Text>Hello world!</Text>
+      <Toggle settingsKey="displaySeconds" label="Display Seconds"/>
+      <Toggle settingsKey="displayBattery" label="Display Battery"/>
+      <Toggle settingsKey="steps" label="Display Steps"/>
+      <Toggle settingsKey="heart" label="Display Heart Rate"/>
+      <Toggle settingsKey="cals" label="Display Calories"/>
+
+      <Select
+        label="Date Format"
+        settingsKey="dateFormat"
+        options=
+          {
+            DATE_FORMATS.map(name => ({name}))
+          }
+      />
+    </Section>
+
+    <Section>
+      <Text>UI Color</Text>
+        <ColorSelect
+          settingsKey="color"
+          colors=
+            {[
+              {color: "aliceblue"},
+              {color: "orchid"},
+              {color: "palegoldenrod"},
+              {color: "bisque"},
+              {color: "coral"},
+              {color: "darkorange"},
+              {color: "greenyellow"},
+              {color: "lavender"},
+              {color: "lavenderblush"},
+              {color: "snow"},
+              {color: "white"},
+              {color: "whitesmoke"},
+              {color: "yellow"}
+            ]}
+        />
     </Section>
   </Page>
 ));
