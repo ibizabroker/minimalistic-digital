590fec5dea4f85c4613cc9c80f456d2224b657d3
tarun-gowda <tarun.gowda2000@gmail.com>
2021-12-05 13:15:44 +0530

    constants

---
+++ common/constants.js
@@ -0,0 +1,35 @@
+export const APP_NAME = 'Minimalistic Digital';
+
+export const DATE_FORMAT_MM_DD_YYYY = '12/31/2021';
+export const DATE_FORMAT_DD_MM_YYYY = '31/12/2021';
+export const DATE_FORMAT_MON_DD = 'DEC 31';
+export const DATE_FORMAT_DD_MON = '31 DEC';
+
+export const DATE_FORMATS = [
+  DATE_FORMAT_MM_DD_YYYY,
+  DATE_FORMAT_DD_MM_YYYY,
+  DATE_FORMAT_MON_DD,
+  DATE_FORMAT_DD_MON,
+];
+
+export const FILENAME = "minimalistic.txt";
+export const FILETYPE = "json";
+export const KEY_COLOR = "color";
+export const KEY_DISPLAY_BATTERY = "displayBattery";
+export const KEY_DATE_FORMAT = "dateFormat";
+export const KEY_UI_STATE = "uiState";
+export const KEY_UI_STATE_HEART = "heart";
+export const KEY_UI_STATE_STEPS = "steps";
+export const KEY_UI_STATE_CALS = "cals";
+export const KEY_UI_STATE_FLOORS = "floors"
+
+export const DEFAULT_MODEL = {
+  [KEY_COLOR]: "blue",
+  [KEY_DISPLAY_BATTERY]: true,
+  [KEY_DATE_FORMAT]: {selected: 2},
+  [KEY_UI_STATE]: KEY_UI_STATE_HEART,
+  [KEY_UI_STATE_HEART]: true,
+  [KEY_UI_STATE_STEPS]: true,
+  [KEY_UI_STATE_CALS]: true,
+  [KEY_UI_STATE_FLOORS]: true
+};
